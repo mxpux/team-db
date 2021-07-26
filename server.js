cfbbb86d6fe712ddd0c9fc94cf2d67ef4bced50d
MPx <hellomariap@gmail.com>
2021-07-26 15:42:07 -0700

    Added required files

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mysql = require('mysql2');
+const PORT = process.env.PORT || 3001;
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        user: "root",
+        password: "hello",
+        database: "Employee_Tracker"
+    },
+    console.log("--Connected--")
+);
