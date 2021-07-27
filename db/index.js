9314e93573de408af974f7af188c49d85c61d4c5
MPx <hellomariap@gmail.com>
2021-07-26 19:28:33 -0700

    Added trial functions and queries

--- db/index.js
+++ db/index.js
@@ -20,13 +20,70 @@ class DB {
         return this.connection.query(
             `
         SELECT
-            department.id,
+            role.id,
+            role.title,
+            role.salary,
             department.name
         FROM
-             department
+            role 
+        LEFT JOIN 
+            department ON role.department_id = department.id
         ORDER BY
              department.id;
         `
+        );
+    }
+
+    // viewAllEmp() {
+    //     return this.connection.query(
+    //         `   
+    //     SELECT
+    //         employee.id,
+    //         employee.first_name,
+    //         employee.last_name,
+    //         role.id,
+    //         department.name,
+    //         employee.manager_id,
+    //     FROM
+    //         employee 
+    //     INNER JOIN 
+    //         role ON employee.role = role.id
+    //     INNER JOIN
+    //         department ON role.department_id = department.id,
+    //     ORDER BY
+    //          department.id;
+    //     `
+    //     );
+    // }
+
+    addDepartment(department) {
+        return this.connection.query(
+            `
+        INSERT INTO
+            department
+        SET
+            ?
+        `, department
+        );
+    }
+    addEmp(employee) {
+        return this.connection.query(
+            `
+        INSERT INTO
+            employee
+        SET
+            ?
+        `, employee
+        );
+    }
+    addRole(role) {
+        return this.connection.query(
+            `
+        INSERT INTO
+            role
+        SET
+            ?
+        `, role
         );
     }
 
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const inquirer = require("inquirer");
+const { viewAllEmployee, addEmp, addDepartment } = require("./db");
 const DB = require("./db");
 
 async function mainMenu(){
@@ -8,18 +9,62 @@ async function mainMenu(){
            name: "choice",
            message: "What action would you like to perform?",
            choices: [
-               "View all Departments"
+               "View all Departments", 
+               "View all Roles",
+               "View all Employees",
+               "Add a department",
+               "Add a role",
+               "Add an Employee"
            ] 
        } 
     ])
      switch (choice){
          case "View all Departments":
              return viewAllDepartments()
+         case "View all Roles":
+             return viewAllRoles()
+        case "View all Employees":
+             return viewAllEmp()
+        case "Add a Department":
+             return addDepartment()
+        case "Add an Employee":
+             return addEmp()
+        case "Add a role":
+             return addRole()  
+          
      }
 }
 
 async function viewAllDepartments(){
     const departments = await DB.viewAllDepartments()
     console.table(departments)
+    mainMenu();
 }
-viewAllDepartments();
+async function viewAllRoles(){
+    const roles = await DB.viewAllRoles()
+    console.table(roles)
+    mainMenu();
+}
+async function viewAllEmp(){
+    const emp = await DB.viewAllEmp()
+    console.table(roles)
+    mainMenu();
+}
+
+async function addDepart(){
+    const addDep = await DB.addDepart()
+    console.table(addDepartment)
+    mainMenu();
+}
+async function addEmploy(){
+    const addEmployee = await DB.addEmploy()
+    console.table(addEmp)
+    mainMenu();
+}
+async function addRole(){
+    const addRo = await DB.addRole()
+    console.table(addRole)
+    mainMenu();
+}
+
+mainMenu();
